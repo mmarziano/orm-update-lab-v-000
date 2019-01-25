265b876bedd597aedc54c3125fadb968ecd2ff6f
Monica Marziano <mmarziano1@gmail.com>
2019-01-25 22:28:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,16 +34,17 @@ class Student
   end 
   
   def save
-    
-    
-    
-    sql =<<-SQL
-      INSERT INTO students (name, grade)
-      VALUES (?,?)
-    SQL
-    
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+    if 
+      self.update
+    else 
+      sql =<<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?,?)
+      SQL
+      
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid()")[0][0]
+    end
   end 
     
   
