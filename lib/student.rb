876282595a7d154090aca40c97fc8da37db40e68
Monica Marziano <mmarziano1@gmail.com>
2019-01-26 00:04:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
      DB[:conn].execute(sql,self.name, self.grade, self.id)
     end 
     
-    def self.create  
+    def self.create(name:, grade:)  
       student = self.new 
       self.save
       student 
